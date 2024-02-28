4c67d55e61507b7095ffb8dd5c0d04d93243e569
bart <bartosz.barski@icloud.com>
2024-02-28 11:46:35 +0100

    cleaning goes on

--- __main__.py
+++ __main__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 import menu
+import argparse
+
 
 def main():
+    # parser = argparse.ArgumentParser(description='Do all the things')
+    # parser.add_argument('user_id', type=int,help='the user id')
+    # parser.add_argument('id', type=int,help='the other id')
+    # args = parser.parse_args()
+
     menu.main_menu()
 
 
